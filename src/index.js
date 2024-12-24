3840663a72f977c822d28b24b77159d60ab8e48c
Nirmal Mekala <ndmekala@icloud.com>
2024-12-24 15:53:48 -0600

    misc updates

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const parseArguments = () => {
   const checklistDateArg = process.argv[2];
   const configPath = process.argv[3];
   if (!checklistDateArg || !configPath) {
-    console.log('Usage: checklist-generator <YYYY-MM-DD> </path/to/json>');
+    console.log('Usage: checklist-generator <YYYY-MM> </path/to/json>');
     throw new Error(errorMessages.missingArgs);
   }
   const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
@@ -48,15 +48,12 @@ const buildFormattedChecklistData = (checklistDate, columnData, config) => {
   const NON_DATE_COLUMNS = 4 // task, empty, empty, date/empty
   const TASK_COLUMN_INDEX = 0 // first item
   const DATE_COLUMN_L_SPACING = 2 // task, empty
-
   // Prepare the empty row for CSV
   let emptyRow = Array(dateUtils.daysInMonth(checklistDate) + NON_DATE_COLUMNS).fill('');
-
   // Create header row
   let r1 = [...emptyRow];
   r1[TASK_COLUMN_INDEX] = 'task';
   r1[r1.length - 1] = `${dateUtils.monthString(checklistDate).slice(0, 3)} ${dateUtils.yearString(checklistDate).slice(-2)}`;
-
   // Create rows for date and day
   let r3 = [...emptyRow];
   let r4 = [...emptyRow];
@@ -64,8 +61,7 @@ const buildFormattedChecklistData = (checklistDate, columnData, config) => {
     r3[i + DATE_COLUMN_L_SPACING] = dateInfo.date;
     r4[i + DATE_COLUMN_L_SPACING] = dateInfo.day;
   });
-
-  // Format checklist data
+  // Build checklist array
   let formattedChecklistData = [r1, [...emptyRow], r3, r4, [...emptyRow]];
   config.tasks.forEach((task) => {
     if (!task.when.length || task.when.includes(dateUtils.monthString(checklistDate))) {
